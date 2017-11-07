93ac6a554d9185f103a92cdacd0a5496bb65632e
Hadas Tamás <hadastk@gmail.com>
2017-11-07 21:57:29 +0100

    create common config files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const examples = ['mailbox', 'todos', 'tree'];
+
+module.exports = examples.map(function(example) {
+    return {
+        entry: './' + example + '/src/index.tsx',
+        output: {
+            path: path.resolve(__dirname, example + '/dist'),
+            filename: 'bundle.js'
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/
+                }
+            ]
+        }
+    };
+});
